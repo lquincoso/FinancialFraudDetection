0ffaa5402a8548a66850739bff35c9aeed1ca25e
Ibsen martinez <122346484+Imart182@users.noreply.github.com>
2024-09-28 18:42:04 -0400

    dashboard
    Fixed some logic issues with the user inputs

--- DashBoard.py
+++ DashBoard.py
@@ -2,11 +2,16 @@ import streamlit as st
 import requests
 import datetime
 import json
+import re
 import mysql.connector
 
 
 
+
 st.title("Financial fraud detection")
+
+randomDigits = r"^c\d{5}$"
+
 
 
 # Create a form for inputting transaction details
@@ -15,56 +20,59 @@ st.header("Enter transaction details")
 # Input fields for the transaction form
 with st.form(key="transaction_form"):
  transaction_date = st.date_input("Transaction date", value=datetime.date.today())
- type = st.selectbox("Transaction type", [" ", "CASH-IN", "CASH-OUT", "DEBIT", "PAYMENT", "TRANSFER"])
+ type = st.selectbox("Transaction type", [" --SELECT A TYPE-- ", "CASH-IN", "CASH-OUT", "DEBIT", "PAYMENT", "TRANSFER"])
  amount = st.number_input("Transaction amount", min_value=0.0, step=0.01)
  oldbalanceOrg = st.number_input("Initial balance", min_value=0.0, step=0.01)
  newbalanceOrig = st.number_input("New balance", min_value=0.0, step=0.01)
  oldbalanceDest = st.number_input("Recipient's Initial balance", min_value=0.0, step=0.01)
  newbalanceDest = st.number_input("Recipient's new balance", min_value=0.0, step=0.01)
- nameOrig = st.text_input("Sender Name")
- nameDest = st.text_input("Receiver Name")
+ nameOrig = st.text_input("Initiator Name") # must be c and 5 unique numbers
+ nameDest = st.text_input("Recipient Name") # must be c and 5 random numbers
  submit = st.form_submit_button("Submit")
 
 
 # Button to submit the transaction
 if submit:
-    new_transaction = {
-        "nameOrig": nameOrig,
-        #"steps": steps,
-        "type": type,
-        "amount": amount,
-        "oldbalanceOrg": oldbalanceOrg,
-        "newbalanceOrig": newbalanceOrig,
-        "nameDest": nameDest,
-        "oldbalanceDest": oldbalanceDest,
-        "newbalanceDest": newbalanceDest,
-        #"isfraud": isfraud,
-        #"isflaggedFraud": isflaggedFraud
-    }
-    url = 'http://10.108.94.53:5000/api/transaction'  # Replace with your actual API endpoint
-    headers = {'Content-Type': 'application/json'}
-
-    try:
-        response = requests.post(url, headers=headers, data=json.dumps(new_transaction))
-
-        # Check the response
-        if response.status_code == 200:
-            st.success('Data sent successfully!')
-        else:
-            st.error(f"Failed to send data. Status code: {response.status_code}")
-            st.error(f"Response text: {response.text}")  # Log the response content for more debugging info
-
-    except requests.exceptions.RequestException as e:
-        st.error(f"An error occurred: {str(e)}")
-    
-
-    #json_data = json.dumps(new_transaction, indent=4)
-
-    st.header("Submitted Transaction in JSON Format")
-    st.json(new_transaction)
-    
-    if  amount >= 0 and oldbalanceOrg >= 0 and newbalanceOrig >= 0 and oldbalanceDest >= 0 and newbalanceDest >= 0 and type != " ":
+    if  amount >= 0 and oldbalanceOrg >= 0 and newbalanceOrig >= 0 and oldbalanceDest >= 0 and newbalanceDest >= 0 and nameOrig  and nameDest and re.match(randomDigits, nameOrig) and re.match(randomDigits, nameDest)and type != " --SELECT A TYPE-- ":
+        
         # Display a summary of the transaction
+        new_transaction = {
+            "nameOrig": nameOrig,
+            #"steps": steps,
+            "type": type,
+            "amount": amount,
+            "oldbalanceOrg": oldbalanceOrg,
+            "newbalanceOrig": newbalanceOrig,
+            "nameDest": nameDest,
+            "oldbalanceDest": oldbalanceDest,
+            "newbalanceDest": newbalanceDest,
+            #"isfraud": isfraud,
+            #"isflaggedFraud": isflaggedFraud
+        }
+        url = 'http://10.108.94.53:5000/api/transaction'  # Replace with your actual API endpoint
+        headers = {'Content-Type': 'application/json'}
+
+        try:
+            response = requests.post(url, headers=headers, data=json.dumps(new_transaction))
+
+            # Check the response
+            if response.status_code == 200 or response.status_code == 201:
+                st.success('Data sent successfully!')
+            else:
+                st.error(f"Failed to send data. Status code: {response.status_code}")
+                st.error(f"Response text: {response.text}")  # Log the response content for more debugging info
+
+        except requests.exceptions.RequestException as e:
+            st.error(f"An error occurred: {str(e)}")
+    
+
+        #json_data = json.dumps(new_transaction, indent=4)
+
+        st.header("Submitted Transaction in JSON Format")
+        st.json(new_transaction)
+
+
+
         st.success(f"Transaction Submitted Successfully!")
         st.write(f"**Date:** {transaction_date}")
         st.write(f"**Transaction Type:** {type}")
@@ -78,7 +86,7 @@ if submit:
         # Optionally, add the transaction to a list or a database
         # You could store these details in a database or process further
         # transactions.append({"sender": sender, "receiver": receiver, "amount": amount, "date": transaction_date})
-    elif amount >= 0 and oldbalanceOrg >= 0 and newbalanceOrig >= 0 and oldbalanceDest >= 0 and newbalanceDest >= 0 and type == " ":
+    elif amount >= 0 and oldbalanceOrg >= 0 and newbalanceOrig >= 0 and oldbalanceDest >= 0 and newbalanceDest >= 0 and type == " --SELECT A TYPE-- ":
              st.error("Please select a transaction type")      
     else:
         st.error("Please fill in all fields with the appropriate values.")
